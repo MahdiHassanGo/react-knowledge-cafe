a0f7f1b64c56f53e76bf43cb7245be95a48c4165
MahdiHassanGo <contact.mahdiasif@gmail.com>
2024-10-22 01:34:58 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import Blogs from "../Blogs/Blogs";
+
+const Header = () => {
+    return (
+        <div>
+      <h1 className='text-5xl bg-blue-300 font-bold'>Knowledge Cafe</h1>
+
+      <Blogs></Blogs>
+      </div>
+    );
+};
+
+export default Header;
